b1d965db3e6d60c133b6c6608a69445611701164
Yaroslav Svystun <111186021+eragon2193@users.noreply.github.com>
2023-03-28 09:43:41 +0200

    first commit

---
+++ src/menu.js
@@ -0,0 +1,49 @@
+export default function MenuModule() {
+const content = document.getElementById('content')
+const menuItems = [
+  {
+    name: 'Dragon Roll Sushi', 
+    price: '12.50E',
+    image: 'sushi', },
+  {
+    name:'Spicy Dragon Bowl',
+    price: '13.00E',
+    image: 'bowl'
+  },
+  {
+    name: 'Dragon Chicken',
+    price: '14.00E',
+    image:  'chicken' 
+  },
+  { 
+    name:'Dragon Fruit Bowl',
+    price:'12.00E',
+    image: 'fruit'
+  }
+]
+const menu = document.createElement('div')
+menu.classList.add('menu')
+const createFoodCard = (type) =>{
+  const card = document.createElement('div')
+  card.classList.add('food-card')
+  const foodText = document.createElement('div')
+  foodText.classList.add('food-text')
+  const foodName = document.createElement('h3')
+  foodName.innerText = type.name
+  const foodPrice = document.createElement('p')
+  foodPrice.innerText = type.price
+  const img = document.createElement('img')
+  img.src = `../src/${type.image}.jpg`
+  foodText.append(foodName,foodPrice)
+  card.append(foodText, img)
+  menu.append(card)
+}
+menuItems.forEach((x) => {
+  createFoodCard(x)
+})
+content.append(menu)
+}
+
+// to do: 
+// loop for card links and texts for them
+// append all on each call
